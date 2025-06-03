8a8c6ed14c35efdff868110ef22574415f55f122
Mohan Krishna <142295454+mohan13krishna@users.noreply.github.com>
2025-06-03 10:49:39 +0530

    Updated script.js with new features

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const APIURL = 'https://api.github.com/users/'
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
-
+ 
 async function getUser(username) {  
     try {
         const { data } = await axios(APIURL + username)
